953726d156b16b96e8eef87834c887bde3d86388
Robert Bishop <rbishop2006@gmail.com>
2020-02-08 15:26:11 -0800

    adding some info before menu

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+  $(".navTabs").on("click", "a", function() {
+    $(".tabItems>p").removeClass("show")
+    $("a").removeClass("active")
+    $(this).addClass("active")
+    $(this)
+      .find("+ .tabItems>p")
+      .addClass("show")
+  })
+})
