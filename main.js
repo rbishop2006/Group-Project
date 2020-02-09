217dd20dfeeaf1fede816bf3cb831437f157acf1
Robert Bishop <rbishop2006@gmail.com>
2020-02-08 21:57:25 -0800

    menufixed

--- main.js
+++ main.js
@@ -7,4 +7,48 @@ $(document).ready(function() {
       .find("+ .tabItems>p")
       .addClass("show")
   })
+  $.get("https://obscure-tundra-54269.herokuapp.com/casual-dining", function(
+    food
+  ) {
+    var apps = food.appetizers.map(function(item) {
+      return `
+      <p class="appName">${item.name}</p>
+     
+      <p class="appDescription">${item.description}</p>
+      <img class="menuIcons" src="assetsCasualRest/iconAllergy.png">
+      <img class="menuIcons" src="assetsCasualRest/iconFav.png">
+      <img class="menuIcons" src="assetsCasualRest/iconSpicy.png">
+      <img class="menuIcons" src="assetsCasualRest/iconVegan.png">
+      <p class="appPrice">-$${item.price}-</p>`
+    })
+
+    var entrees = food.entrees.map(function(item) {
+      return `
+      <p class="entreeName">${item.name}</p>
+      
+      <p class="entreeDescription">${item.description}</p>
+      <img class="menuIcons" src="assetsCasualRest/iconAllergy.png">
+      <img class="menuIcons" src="assetsCasualRest/iconFav.png">
+      <img class="menuIcons" src="assetsCasualRest/iconSpicy.png">
+      <img class="menuIcons" src="assetsCasualRest/iconVegan.png">
+      <p class="entreePrice">-$${item.price}-</p>`
+    })
+
+    var desserts = food.desserts
+      .map(function(item) {
+        return `
+        <p class="dessertsName">${item.name}</p>
+        
+        <p class="dessertsDescription">${item.description}</p>
+        <img class="menuIcons" src="assetsCasualRest/iconAllergy.png">
+        <img class="menuIcons" src="assetsCasualRest/iconFav.png">
+        <img class="menuIcons" src="assetsCasualRest/iconSpicy.png">
+        <img class="menuIcons" src="assetsCasualRest/iconVegan.png">
+        <p class="dessertsPrice">-$${item.price}-</p>`
+      })
+      .join("")
+    $("#appMenu").html(apps)
+    $("#entreesMenu").html(entrees)
+    $("#dessertsMenu").html(desserts)
+  })
 })
